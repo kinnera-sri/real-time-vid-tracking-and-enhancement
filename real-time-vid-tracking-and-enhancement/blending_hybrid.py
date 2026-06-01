0d97ff20718f457ca3dac05b26a4e143e67a9074
Kinnera <myla.kinnera.sri@gmail.com>
2026-06-01 12:22:40 +0530

    re-did feather blending

--- real-time-vid-tracking-and-enhancement/blending_hybrid.py
+++ real-time-vid-tracking-and-enhancement/blending_hybrid.py
@@ -555,9 +555,9 @@ class PlanarARTracker:
             return
 
         # Setup save targets directories
-        output_dir = "./real-time-vid-tracking-and-enhancement/tracked_frames_lego"
+        output_dir = "./real-time-vid-tracking-and-enhancement/tracked_frames_hybrid"
         os.makedirs(output_dir, exist_ok=True)
-        log_filepath = "./real-time-vid-tracking-and-enhancement/tracking_log_lego.txt"
+        log_filepath = "./real-time-vid-tracking-and-enhancement/tracking_log_hybrid.txt"
 
         cv2.namedWindow(self.win_live, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(self.win_live, 960, 540)
